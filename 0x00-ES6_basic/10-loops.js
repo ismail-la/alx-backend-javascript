243adf5169789b5507622c6041656fb08536c2c8
ismail-la <lahbariismail@gmail.com>
2024-01-04 02:26:41 +0100

    0x00. ES6 Basics

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,9 +2,9 @@
 /* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
   const mynewarray = [];
-// eslint-disable-next-line no-restricted-syntax
   for (const item of array) {
     mynewarray.push(appendString + item);
   }
+
   return mynewarray;
 }
