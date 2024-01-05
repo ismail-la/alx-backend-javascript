b351280095d78a8314e53eb2ecb9ade59263a3a4
ismail-la <lahbariismail@gmail.com>
2024-01-05 02:39:48 +0100

    0x02. ES6 classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,72 +1,38 @@
-/**
- * Implement a class named HolbertonCourse
- */
 export default class HolbertonCourse {
-  /**
-   * Creates a new @link HolbertonCourse
-   *
-   * @param {String} name - Name of the course
-   * @param {Number} length - How long the course is (in months)
-   * @param {String[]} students - Names of students in the course
-   */
- 
   constructor(name, length, students) {
+    if (typeof name !== 'string') throw new TypeError('name must be a string');
+    if (!Number.isInteger(length)) throw new TypeError('length must be a number');
+    if (!Array.isArray(students)) throw new TypeError('students type must be an Array');
+
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
-  /**
-   * Gets the name of this course
-   */
   get name() {
     return this._name;
   }
 
-  /**
-   * Sets the name of this course
-   */
-  set name(val) {
-    if (typeof val !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = val;
+  set name(value) {
+    if (typeof value !== 'string') throw new TypeError('name must be a string');
+    this._name = value;
   }
 
-  /**
-   * Gets the length of this course (in months)
-   */
   get length() {
     return this._length;
   }
 
-  /**
-   * Sets the length of this course (in months)
-   */
-  set length(val) {
-    if (typeof val !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    this._length = val;
+  set length(value) {
+    if (!Number.isInteger(value)) throw new TypeError('length must be a number');
+    this._length = value;
   }
 
-  /**
-   * Gets the names of students in this course
-   */
   get students() {
     return this._students;
   }
 
-  /**
-   * Sets the names of students in this course
-   */
-  set students(val) {
-    if (!(val instanceof Array)) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    if (!val.every((student) => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    this._students = val;
+  set students(value) {
+    if (!Array.isArray(value)) throw new TypeError('students type must be an Array');
+    this._students = value;
   }
 }
