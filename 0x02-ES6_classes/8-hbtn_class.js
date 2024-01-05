8d7c5386f1cb61aedfec77f493e6e5cfae707dff
ismail-la <lahbariismail@gmail.com>
2024-01-05 03:06:19 +0100

    0x02. ES6 classes

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,16 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
+    }
+    if (hint === 'string') {
+      return this.location;
+    }
+    return null;
+  }
+}
