b07b0ee672c1a0a7ec85a72aa040358fa5fe8ca3
ismail-la <lahbariismail@gmail.com>
2024-02-27 22:54:48 +0100

    update

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -39,7 +39,7 @@ function countStudents(fileName) {
             output += `List: ${students[key].join(', ')}\n`;
           }
         }
-        resolve(output);
+        res(output);
       }
     });
   });
