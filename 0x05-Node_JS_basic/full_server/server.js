fb07f1ab0bc6d0d0289942e34938cafcdd62e2a3
ismail-la <lahbariismail@gmail.com>
2024-02-27 23:15:21 +0100

    update

---
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = require('./routes/index');
+
+const app = express();
+const PORT = 1245;
+
+app.use('/', router);
+app.use('/students', router);
+app.use('/students/:major', router);
+
+app.listen(PORT);
+
+export default app;
