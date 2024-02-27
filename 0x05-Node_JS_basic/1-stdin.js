10ac85bf5241d5b43cbd1970c1605655d60244eb
ismail-la <lahbariismail@gmail.com>
2024-02-27 21:55:15 +0100

    update

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+// Program named 1-stdin.js that will be executed through command line
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
