922d287d8e88281288aabf16f89c391f5f6dd7b0
ismail-la <lahbariismail@gmail.com>
2024-02-27 21:48:09 +0100

    update

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,7 @@
 // Function named displayMessage that prints in STDOUT the string argument
 
-function displayMessage = (message) => console.log(message);
+function displayMessage(message) {
+  console.log(message);
+}
 
 module.exports = displayMessage;
