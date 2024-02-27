76c17725f23a1333644d23e5b29b9e2d9dd6cc8f
ismail-la <lahbariismail@gmail.com>
2024-02-27 21:56:55 +0100

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,6 +6,6 @@ process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
 });
 
-process.stdin.on('end', () => {
+process.stdin.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
 });
