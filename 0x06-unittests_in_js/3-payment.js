2794e1cd3268e0c9602bdb7e3b198b10d19abae0
ismail-la <lahbariismail@gmail.com>
2024-03-01 01:11:08 +0100

    0x06. Unittests in JS

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -2,7 +2,7 @@ const utils = require('./utils');
 
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
   const totalCost = utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${totalCost}`);
+  console.log(`The total is: ${total}`);
 };
 
 module.exports = sendPaymentRequestToApi;
