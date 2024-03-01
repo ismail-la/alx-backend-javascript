08aa41dc76e8bc2250e23fcc2ca0cb235d88ab86
ismail-la <lahbariismail@gmail.com>
2024-03-01 01:46:04 +0100

    0x06. Unittests in JS

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -5,7 +5,7 @@ describe('API integration test', () => {
   const URL_API = 'http://localhost:7865';
 
   it('GET / returns correct response', (done) => {
-    request.get(`${API_URL}/`, (_err, res, body) => {
+    request.get(`${URL_API}/`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
       done();
